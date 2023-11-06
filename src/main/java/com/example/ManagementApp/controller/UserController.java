04ab404b7e3be6de7b5a62619330cb6ec32f2bb5
Edin Ganić <edinganic99@gmail.com>
2023-11-06 12:55:05 +0100

    added_getUserById

--- src/main/java/com/example/ManagementApp/controller/UserController.java
+++ src/main/java/com/example/ManagementApp/controller/UserController.java
@@ -1,17 +1,22 @@
 package com.example.ManagementApp.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.ManagementApp.exception.UserNotFoundException;
 import com.example.ManagementApp.model.User;
 import com.example.ManagementApp.repository.UserRepository;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
@@ -25,6 +30,12 @@ public class UserController {
     @GetMapping("/users")
     List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+        .orElseThrow(() -> new UserNotFoundException(id));
     }
     
 }
